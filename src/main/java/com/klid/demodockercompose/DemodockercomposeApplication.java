2155e2d0ccfabdfe626eb74656ddf4cc99a46581
Ivan Kaptue <ivanokaptue@gmail.com>
2021-04-06 17:43:44 +0100

    commit

---
+++ src/main/java/com/klid/demodockercompose/DemodockercomposeApplication.java
@@ -0,0 +1,13 @@
+package com.klid.demodockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodockercomposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemodockercomposeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/klid/demodockercompose/DemodockercomposeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klid.demodockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodockercomposeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
